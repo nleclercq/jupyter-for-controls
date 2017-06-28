c5e6dae55ae11b554340371f68d2767e329d512e
Nicolas Leclercq <nicolas.leclercq@synchrotron-soleil.fr>
2017-06-28 19:14:21 +0200

    more tests and studies

--- common/bokeh_session.py
+++ common/bokeh_session.py
@@ -35,7 +35,6 @@ class BokehSessionHandler(Handler):
 
     def on_session_created(self, session_context):
         print("SessionHandler: on_session_created <<")
-        BokehServer.print_info(True)
         print("SessionHandler: on_session_created >>")
 
     def on_session_destroyed(self, session_context):
@@ -139,7 +138,7 @@ class BokehServer(object):
             pass
         prd = session.callback_period
         if prd is not None:
-            session._doc.add_periodic_callback(pcb, max(250, 1000. * prd))
+            session._doc.add_periodic_callback(pcb, max(1, 1000. * prd))
         
     @staticmethod
     def open_session(new_session):
@@ -180,107 +179,4 @@ class BokehServer(object):
         except Exception as e:
             print(e)
 
-
-class MySessionExample(BokehSession):
-    
-    def __init__(self):
-        BokehSession.__init__(self)
-        self.callback_period = 1.
-        self._np = 100
-        self._widgets_layout = None
-        columns = dict()
-        columns['x'] = self._gen_x_scale()
-        columns['y'] = self._gen_y_random_data()
-        self._cds = ColumnDataSource(data=columns)
-
-    def _gen_x_scale(self):
-        """x data"""
-        return np.linspace(1, self._np, num=self._np, endpoint=True)
-    
-    def _gen_y_random_data(self):
-        """y data"""
-        return np.random.rand(self._np)
-    
-    def __on_update_period_change(self, attr, old, new):
-        """called when the user changes the refresh period using the dedicated slider"""
-        self.callback_period = new
-        
-    def __on_num_points_change(self, attr, old, new):
-        """called when the user changes the number of points using the dedicated slider"""
-        self._np = int(new)
-
-    def setup_model(self):
-        """setup the session model then return it"""
-        # a slider to control the update period
-        rrs = Slider(start=0.25, 
-                     end=2, 
-                     step=0.25, 
-                     value=self.callback_period, 
-                     title="Updt.period [s]",)
-        rrs.on_change("value", self.__on_update_period_change)
-        # a slider to control the number of points
-        nps = Slider(start=0, 
-                     end=1000, 
-                     step=10, 
-                     value=self._np, 
-                     title="Num.points")
-        nps.on_change("value", self.__on_num_points_change)
-        # the figure and its content
-        p = figure(plot_width=650, plot_height=200)
-        p.toolbar_location = 'above'
-        p.line(x='x', y='y', source=self._cds, color="navy", alpha=0.5)
-        # widgets are placed into a dedicated layout
-        self._widgets_layout = widgetbox(nps, rrs)
-        # arrange all items into a layout then return it as the session model
-        return layout([[self._widgets_layout, p]])
-    
-    def periodic_callback(self):
-        """periodic activity"""
-        self._cds.data.update(x=self._gen_x_scale(), y=self._gen_y_random_data())
-
-
-class MyExtendedSessionExample(MySessionExample):
-    
-    def __init__(self):
-        MySession.__init__(self)
-        self._suspend_resume_button = None
-
-    def setup_model(self):
-        model = super(MyExtendedSession, self).setup_model()
-        cb = Button(label='close')
-        cb.on_click(self.close)
-        self._suspend_resume_button = Button(label='suspend')
-        self._suspend_resume_button.on_click(self.suspend_resume)
-        self._widgets_layout.children.append(self._suspend_resume_button)
-        self._widgets_layout.children.append(cb)
-        return model
-    
-    def periodic_callback(self):
-        print('MyExtendedSession.periodic_callback <<')
-        super(MyExtendedSession, self).periodic_callback()
-        print('MyExtendedSession.periodic_callback >>')
-   
-    def suspend_resume(self): 
-        # callback period == None means callback disabled (i.e. suspended)
-        if self.callback_period is None:
-            self._suspend_resume_button.label = 'suspend'
-            self.resume()
-        else:
-            self._suspend_resume_button.label = 'resume'
-            self.pause()
-        
-    def close(self):  
-        """overwrites super.close - tries to cleanup everything properly"""
-        try:
-            # clear document content (i.e. remove roots)
-            self._doc.clear()
-        except Exception as e:
-            print(e)
-        try:  
-            # close the session (will remove all callbacks)
-            BokehServer.close_session(self)
-        except Exception as e:
-            print(e) 
-        # finally clear cell outputs - e.g. logging (this is an ipython call - not a bokeh one)
-        clear_output()
 
