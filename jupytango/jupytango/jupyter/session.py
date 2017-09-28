f184210508849cac20ad303d65fbf96b476af95b
Nicolas Leclercq <nicolas.leclercq@synchrotron-soleil.fr>
2017-09-29 00:26:23 +0200

    much better session impl.

--- jupytango/jupytango/jupyter/session.py
+++ jupytango/jupytango/jupyter/session.py
@@ -21,12 +21,15 @@ from __future__ import print_function
 
 import imp
 import logging
-from threading import Lock
+import socket
+from threading import Lock, Condition
 from collections import deque
 from uuid import uuid4
 
 from IPython.display import HTML, display, publish_display_data
 
+from tornado.ioloop import IOLoop
+        
 from bokeh.io import output_notebook, curstate, show
 from bokeh.resources import Resources
 from bokeh.application import Application
@@ -34,13 +37,15 @@ from bokeh.application.handlers import Handler, FunctionHandler
 from bokeh.embed import server_document
 from bokeh.util.notebook import EXEC_MIME_TYPE, HTML_MIME_TYPE
 
+from bokeh.server.server import Server
+        
 try:
-    from fs.client.jupyter.tools import JupyterContext, get_jupyter_context
+    from fs.client.jupyter.tools import JupyterContext, get_jupyter_context, NotebookCellContent
 except:
     try: 
-        from jupytango.jupyter.tools import JupyterContext, get_jupyter_context
+        from jupytango.jupyter.tools import JupyterContext, get_jupyter_context, NotebookCellContent
     except:
-        from common.tools import JupyterContext, get_jupyter_context
+        from common.tools import JupyterContext, get_jupyter_context, NotebookCellContent
         
 module_logger_name = "fs.client.jupyter.session"
 
@@ -64,16 +69,20 @@ class BokehSessionHandler(Handler):
 
 # ------------------------------------------------------------------------------
 class BokehSession(object):
+    
     __repo__ = dict()
     __repo_lock__ = Lock()
 
-    __logger__ = logging.getLogger(module_logger_name)
-
+    __sessions__ = deque()
+    __sessions_lock__ = Lock()
+    
     def __init__(self, uuid=None):
         # session identifier
         self._uuid = uuid if uuid else str(uuid4().hex)
+        # logger
+        self._logger = NotebookCellContent(self._uuid, logger=logging.getLogger(module_logger_name)) 
         # the session info
-        self._info = None
+        self._server_info = dict()
         # the associated bokeh document (for experts only)
         self._doc = None
         # periodic callback period in seconds - defaults to None (i.e. disabled)
@@ -84,33 +93,28 @@ class BokehSession(object):
         self._closed = False
         # close existing session: this is a way to avoid leaks & resources waste
         if uuid is not None:
-            self.__close_existing_session(uuid)
+            self.__close_existing_session()
         # insert new session into the repo
         with BokehSession.__repo_lock__:
             BokehSession.__repo__[self._uuid] = self
         BokehSession.print_repository_status()
-
-    def __close_existing_session(self, uuid):
+    
+    def __close_existing_session(self):
         with BokehSession.__repo_lock__:
             try:
-                session = BokehSession.__repo__[uuid]
+                session = BokehSession.__repo__[self._uuid]
                 session.close()
             except KeyError:
                 pass
             except Exception as e:
-                BokehSession.__logger__.error(e)
+                self._logger.error(e)
             finally:
                 try:
-                    del BokehSession.__repo__[uuid]
+                    del BokehSession.__repo__[self._uuid]
                 except KeyError:
                     pass
                 except Exception as e:
-                    BokehSession.__logger__.error(e)
-
-    def _on_session_created(self, session_info):
-        self._info = session_info
-        self._doc = session_info['document']
-        self.setup_document()
+                    self._logger.error(e)
 
     def _on_session_destroyed(self):
         pass
@@ -163,7 +167,7 @@ class BokehSession(object):
     
     def open(self):
         """open the session"""
-        BokehSessionManager.open_session(self)
+        self.__open()
 
     def close(self, async=True):
         """close the session"""
@@ -184,9 +188,9 @@ class BokehSession(object):
         try:
             if self._doc:
                 self._doc.clear()
-            BokehSessionManager.close_session(self)
+            self.__close()
         except Exception as e:
-            BokehSession.__logger__.error(e)
+            self._logger.error(e)
         finally:
             self._closed = True
             with BokehSession.__repo_lock__:
@@ -258,94 +262,65 @@ class BokehSession(object):
                 try:
                     s.close()
                 except Exception as e:
-                    BokehSession.__logger__.error('failed to close BokehSession:{}'.format(s.uuid))
-
-    @staticmethod
-    def print_repository_status():
-        with BokehSession.__repo_lock__:
-            if len(BokehSession.__repo__):
-                BokehSession.__logger__.info(
-                    'BokehSession.repository. contains {} session(s):'.format(len(BokehSession.__repo__)))
-                for s in BokehSession.__repo__.values():
-                    BokehSession.__logger__.info('- {}'.format(s))
-            else:
-                BokehSession.__logger__.info('BokehSession.repository is empty')
-
-
-# ------------------------------------------------------------------------------
-class BokehSessionManager(object):
+                   self._logger.error('failed to close BokehSession:{}'.format(s.uuid))
     
-    __lock__ = Lock()
-
-    __sessions__ = deque()
-    __sessions_lock__ = Lock()
-
-    __logger__ = logging.getLogger(module_logger_name)
-
-    @staticmethod
-    def open_session(new_session):
-        BokehSessionManager.__logger__.debug("BokehSessionManager.open_session <<")
-        assert (isinstance(new_session, BokehSession))
-        with BokehSessionManager.__lock__:
-            BokehSessionManager.__logger__.debug("BokehSessionManager.open_session.spawning server")
-            srv_info = BokehSessionManager.__spawn_server()
-            BokehSessionManager.__logger__.debug("BokehSessionManager.open_session.server spawn")
-        with BokehSessionManager.__sessions_lock__:
-            srv_info['session'] = new_session
-            BokehSessionManager.__sessions__.appendleft(srv_info)
-        script = server_document(url=srv_info['server_url'])
-        if get_jupyter_context() == JupyterContext.LAB:
-            BokehSessionManager.__logger__.info("BokehSessionManager.open_session:running in JupyterContext.LAB")
-            data = {HTML_MIME_TYPE: script, EXEC_MIME_TYPE: ""}
-            metadata = {EXEC_MIME_TYPE: {"server_id": srv_info['server_id']}}
-            publish_display_data(data, metadata=metadata)
-        else:
-            BokehSessionManager.__logger__.info("BokehSessionManager.open_session:running in JupyterContext.NOTEBOOK")
-            display(HTML(script))
-        BokehSessionManager.__logger__.debug("BokehSessionManager.open_session >>")
-
-    @staticmethod
-    def __spawn_server():
-        import socket
-        from tornado.ioloop import IOLoop
-        from bokeh.server.server import Server
+    def __open(self):
+        with BokehSession.__sessions_lock__:
+            self._logger.debug("BokehSessionManager.open_session.spawning server for session {}".format(self.uuid))
+            self.__spawn_server()
+            script = server_document(url=self._server_info['server_url'])
+            if get_jupyter_context() == JupyterContext.LAB:
+                self._logger.info("BokehSessionManager.open_session:running in JupyterContext.LAB")
+                data = {HTML_MIME_TYPE: script, EXEC_MIME_TYPE: ""}
+                metadata = {EXEC_MIME_TYPE: {"server_id": self._server_info['server_id']}}
+                publish_display_data(data, metadata=metadata)
+            else:
+                self._logger.info("BokehSessionManager.open_session:running in JupyterContext.NOTEBOOK")
+                display(HTML(script))
+            self._logger.debug("BokehSessionManager.open_session.server spawn for session {}".format(self.uuid))
+          
+    def __spawn_server(self):
         bslg = logging.getLogger('bokeh.server.util')
         bsll = bslg.getEffectiveLevel()
         bslg.setLevel(logging.ERROR) 
         output_notebook(Resources(mode='inline', components=["bokeh", "bokeh-gl"]), hide_banner=True)
-        app = Application(FunctionHandler(BokehSessionManager.__session_entry_point))
+        self._server_info['application'] = app = Application(FunctionHandler(self.__session_entry_point))
         app.add(BokehSessionHandler())
-        srv = Server({'/': app}, io_loop=IOLoop.current(), port=0, allow_websocket_origin=['*'])
-        srv_id = uuid4().hex
+        self._server_info['server'] = srv = Server({'/': app}, io_loop=IOLoop.instance(), port=0, allow_websocket_origin=['*'])
+        self._server_info['server_id'] = srv_id = uuid4().hex
         curstate().uuid_to_server[srv_id] = srv
         srv_addr = srv.address if srv.address else socket.gethostbyname(socket.gethostname())
-        srv_url = 'http://{}:{}/'.format(srv_addr, srv.port)
+        self._server_info['server_url'] = 'http://{}:{}/'.format(srv_addr, srv.port)
         srv.start()
-        bslg.setLevel(bsll) 
-        return {'server': srv, 'server_id': srv_id, 'server_url': srv_url, 'application': app}
+        bslg.setLevel(bsll)
     
-    @staticmethod
-    def __session_entry_point(doc):
+    def __session_entry_point(self, doc):
         try:
-            BokehSessionManager.__logger__.debug("BokehSessionManager.session_entry_point <<")
-            with BokehSessionManager.__sessions_lock__:
-                session_info = BokehSessionManager.__sessions__.pop()
-            session_info['document'] = doc
-            session_info['session']._on_session_created(session_info)
+            with BokehSession.__sessions_lock__:
+                self._logger.debug("BokehSessionManager.session_entry_point for session {}".format(self.uuid))
+                self._doc = doc
+                self.setup_document()
         except Exception as e:
-            BokehSessionManager.__logger__.error(e)
+            self._logger.error(e)
         finally:
-            BokehSessionManager.__logger__.debug("BokehSessionManager.session_entry_point >>")
             return doc
-
-    @staticmethod
-    def close_session(session):
+        
+    def __close(self):
         # TODO: how to release every single resource associated with the session?
-        BokehSessionManager.__logger__.debug("BokehSessionManager.close_session <<")
-        assert (isinstance(session, BokehSession))
-        if session.server:
+        self._logger.debug("BokehSessionManager.closing session {}".format(self.uuid))
+        if self.server:
             try:
-                session.server.stop()
+                self.server.stop()
             except AssertionError as e:
-                pass #TODO: ignore bokeh 0.12.7 "already stopped" error
-        BokehSessionManager.__logger__.debug("BokehSessionManager.close_session >>")
+                # ignore bokeh "already stopped" error
+                pass
+            
+    @staticmethod
+    def print_repository_status():
+        with BokehSession.__repo_lock__:
+            if len(BokehSession.__repo__):
+                print('BokehSession.repository. contains {} session(s):'.format(len(BokehSession.__repo__)))
+                for s in BokehSession.__repo__.values():
+                    print('- {}'.format(s))
+            else:
+                print('BokehSession.repository is empty')
